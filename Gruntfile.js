b30ad259e427056ab8c53efd5abf46e849a34dcc
Jean-Philippe Bergeron <bergeron.jeanphilippe@gmail.com>
2021-07-15 19:12:36 -0400

    first

---
+++ Gruntfile.js
@@ -0,0 +1,67 @@
+module.exports = function( grunt ){
+	grunt.initConfig( {
+		pkg: grunt.file.readJSON( "package.json" ),
+		watch: {
+			options: {
+				livereload: true
+			},
+			jade: {
+				tasks: ["jade:debug"],
+				files: ["**/*.jade", "**/*.md", "!layouts/*.jade"]
+			}
+		},
+		jade: {
+			options: {
+				pretty: true,
+				files: {
+					"*": ["**/*.jade", "!layouts/*.jade"]
+				}
+			},
+			debug: {
+				options: {
+					locals: {
+						livereload: true
+					}
+				}
+			},
+			publish: {
+				options: {
+					locals: {
+						livereload: false
+					}
+				}
+			}
+		},
+		web: {
+			options: {
+				port: 8001
+			}
+		}
+	} );
+
+	grunt.loadNpmTasks( "grunt-contrib-watch" );
+	grunt.loadNpmTasks( "grunt-jade-tasks" );
+
+	grunt.registerTask( "default", ["jade:debug", "web"] );
+	grunt.registerTask( "publish", ["jade:publish"] );
+
+	grunt.registerTask( "web", "Start web server...", function(){
+		var options = this.options();
+		// var connect = require( "connect" );
+		// connect.createServer(
+		// 	connect.static( __dirname )
+		// ).listen( options.port );
+		console.log( "http://localhost:%s", options.port );
+
+		grunt.task.run( ["watch:jade"] );
+	} );
+
+};
+
+
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M master
+git remote add origin https://github.com/jean343/requiempourlaterre.github.io.git
+git push -u origin master
